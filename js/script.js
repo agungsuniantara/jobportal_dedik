2c201e928990a106598880484102306d33680d98
suniantaraa <agungsuniantara@gmail.com>
2024-04-13 10:53:18 +0800

    update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,19 @@ $(document).ready(function () {
     $(".dropdown").toggleClass("active");
   });
 
+  function animateOnScroll() {
+    $(".fade").each(function () {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 200) {
+        $(this).addClass("fade-in");
+      } else {
+        $(this).removeClass("fade-in");
+      }
+    });
+  }
+
   function filterJobs(category) {
     $(".data-filter").hide();
     if (category === "all job") {
@@ -12,6 +25,23 @@ $(document).ready(function () {
     } else if (category === "remote job") {
       $('.data-filter[data-specialist="' + category + '"]').show();
     }
+    else if (category === "featured job") {
+      $('.data-filter[data-specialist="' + category + '"]').show();
+    }
+    else if (category === "high paying job") {
+      $('.data-filter[data-specialist="' + category + '"]').show();
+    }
+    else if (category === "it and software") {
+      $('.data-filter[data-specialist="' + category + '"]').show();
+    }
+    else if (category === "fresh job") {
+      $('.data-filter[data-specialist="' + category + '"]').show();
+    }
+    else if (category === "fresh job") {
+      $('.data-filter[data-specialist="' + category + '"]').show();
+    }
+    
+
   }
 
   $(".search-button").click(function () {
@@ -35,4 +65,18 @@ $(document).ready(function () {
     var category = $(this).find("h5").text().toLowerCase();
     filterJobs(category);
   });
+  animateOnScroll();
+
+  $(window).scroll(function () {
+    animateOnScroll();
+  });
+$(".button-view").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".container-pilihan").offset().top,
+    },
+    300
+    );
+  });
 });
+
